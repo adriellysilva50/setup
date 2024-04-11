d4d0660e0fbb9a18843d044e8e05a61c4328133b
adriellysilva50 <adriellyalbuquerque755@gamil.com>
2024-04-11 11:35:47 -0300

    setup feito e testado

---
+++ setup.bat
@@ -0,0 +1,10 @@
+git config --global user.name "adriellysilva50"
+git config --global user.email "adriellyalbuquerque755@gamil.com"  
+
+cd C:\Users\LETICIADEMELOFIGUEIR\
+  
+git clone https://github.com/adriellysilva50/Ceasar-Cipher.git
+
+cd Ceasar-Cipher
+
+code .
